cf89139b349c597afa8377f42c8978545655ea96
High-Hill <k196336@kansai-u.ac.jp>
2019-03-10 23:58:12 +0900

    3/10 ABC A 2AC

---
+++ abc/a/036_Ocha.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+
+    if(b % a == 0){
+        cout << b / a << endl;
+    }else{
+        cout << (b / a) + 1 << endl;
+    }
+    return 0;
+}
