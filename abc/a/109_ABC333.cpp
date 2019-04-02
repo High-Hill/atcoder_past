3a7310c1d1ced50e170a8a5c96aeff809ca6f275
High-Hill <k196336@kansai-u.ac.jp>
2019-04-02 23:58:50 +0900

    4/2 ABC A 3AC

---
+++ abc/a/109_ABC333.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b;
+    if(a == 2 || b == 2) cout << "No" << endl;
+    else cout << "Yes" << endl;
+    return 0;
+}
