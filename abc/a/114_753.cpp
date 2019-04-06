c4a14a89455e12fa500b417fd19b729a88d28549
High-Hill <k196336@kansai-u.ac.jp>
2019-04-06 23:59:04 +0900

    4/6 ABC A 2AC

---
+++ abc/a/114_753.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin >> x;
+    if(x == 3 || x == 5 || x == 7) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
---
+++ abc/a/115_ChristmasEveEveEve.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int d;
+    cin >> d;
+    cout << "Christmas";
+    for(int i=0;i<25-d;i++) cout << " Eve";
+    return 0;
+}
